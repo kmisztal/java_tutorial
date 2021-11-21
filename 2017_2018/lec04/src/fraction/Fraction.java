71191e9f2ee4b249a93b3516bae4a1f3bb6a8d0b
kmisztal <krzysztof.misztal@gmail.com>
2021-11-21 11:41:57 +0100

    21.11

--- 2017_2018/lec04/src/fraction/Fraction.java
+++ 2017_2018/lec04/src/fraction/Fraction.java
@@ -2,7 +2,30 @@ package fraction;
 
 public class Fraction {
     int numerator;
-    int denominator;
+    int denominator = 1;
+
+    public Fraction(int numerator, int denominator){
+        this.numerator = numerator;
+        if(denominator == 0)
+            throw new ArithmeticException("denominator is zero");
+        this.denominator = denominator;
+        if(this.denominator < 0){
+            this.denominator *= -1;
+            this.numerator *= -1;
+        }
+    }
+
+    public Fraction(int numerator){
+        this(numerator, 1);
+    }
+
+    public static Fraction add(Fraction a, Fraction b){
+        return new Fraction(
+                a.numerator * b.denominator
+                        + b.numerator * a.denominator,
+                a.denominator * b.denominator
+        );
+    }
 
     @Override
     public String toString() {
--- 2017_2018/lec04/src/fraction/Test.java
+++ 2017_2018/lec04/src/fraction/Test.java
@@ -2,6 +2,12 @@ package fraction;
 
 public class Test {
     public static void main(String[] args) {
-
+        Fraction a = new Fraction(1, 2);
+        Fraction b = new Fraction(4);
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+//        a + b;
+        Fraction sum = Fraction.add(a, b);
+        System.out.println(sum);
     }
 }
