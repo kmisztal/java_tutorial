428bbaeacac3c71124c8ad0c70e7a497e4d1b156
kmisztal <krzysztof.misztal@gmail.com>
2021-12-04 15:49:01 +0100

    04.12 poly

---
+++ 2017_2018/qaa/src/Polynomial.java
@@ -0,0 +1,8 @@
+public class Polynomial {
+    public static void main(String[] args) {
+        Polynomial p1 = new Polynomial(1, 1);
+        Polynomial p2 = new Polynomial(1, 1, 1);
+        System.out.println(p1.of(3));
+        System.out.println(p2.of(3));
+    }
+}
